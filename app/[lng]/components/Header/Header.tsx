87380b10092d0f9a988b94124c0277aefddb128c
Artem Lobovskii <lobovskiy@yandex.ru>
2024-08-31 17:57:50 +0300

    feat: show user info, add sign out button

--- app/[lng]/components/Header/Header.tsx
+++ app/[lng]/components/Header/Header.tsx
@@ -1,25 +1,30 @@
-import { AppBar, Stack, Toolbar } from '@mui/material';
+import { AppBar, Box, Chip, IconButton, Stack, Toolbar } from '@mui/material';
+import FaceIcon from '@mui/icons-material/Face';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import { auth } from '@/auth';
+import { initI18n } from '@/app/i18n/i18n';
 import { LanguageToggle } from '@/app/[lng]/components/LanguageToggle/LanguageToggle';
 import { Namespaces } from '@/app/i18n/data/i18n.enum';
 
-import { initI18n } from '@/app/i18n/i18n';
-
+import NavButton from '../../../../components/NavButton';
 import Logo from './components/Logo';
-import NavButton from './components/NavButton';
+import SignOutButton from '@/app/[lng]/components/Header/components/SignOutButton';
+import { protectedNavButtonParams } from '@/app/[lng]/components/Header/constants';
 
 interface Props {
   language: string;
 }
 
 const Header: React.FC<Props> = async ({ language }) => {
+  const session = await auth();
   const ns = Namespaces.HEADER;
   const { t } = await initI18n({ language, namespaces: ns });
 
-  return (
-    <AppBar color="primary" className="" position="static" sx={{ p: 1 }}>
-      <Toolbar sx={{ justifyContent: 'space-between' }}>
-        <Logo />
-        <Stack direction="row" spacing={2} alignItems="center">
+  const renderUserNavigation = () => {
+    if (!session) {
+      return (
+        <>
           <NavButton
             label={t('signIn', { ns })}
             url="/signin"
@@ -30,6 +35,50 @@ const Header: React.FC<Props> = async ({ language }) => {
             url="/register"
             buttonProps={{ variant: 'contained', color: 'secondary' }}
           />
+        </>
+      );
+    }
+
+    return (
+      <>
+        {protectedNavButtonParams.map((navButton) => (
+          <NavButton
+            key={`${navButton.labelHeaderNsKey}${navButton.url}`}
+            label={t(navButton.labelHeaderNsKey, { ns })}
+            url={navButton.url}
+            buttonProps={{
+              variant: 'text',
+              color: 'inherit',
+              sx: { color: 'white' },
+            }}
+          />
+        ))}
+        <Chip
+          icon={<FaceIcon color="inherit" />}
+          label={session.user?.name ?? 'No name'}
+          sx={{ color: 'white', maxWidth: '10rem' }}
+        />
+        <SignOutButton />
+      </>
+    );
+  };
+
+  return (
+    <AppBar color="primary" className="" position="static" sx={{ p: 1 }}>
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
+        <Logo />
+        <Box>
+          <IconButton sx={{ display: { md: 'none' } }}>
+            <MenuIcon />
+          </IconButton>
+        </Box>
+        <Stack
+          direction="row"
+          spacing={2}
+          alignItems="center"
+          sx={{ display: { xs: 'none', md: 'flex' } }}
+        >
+          {renderUserNavigation()}
           <LanguageToggle />
         </Stack>
       </Toolbar>
---
+++ app/[lng]/components/Header/constants.ts
@@ -0,0 +1,10 @@
+export const protectedNavButtonParams = [
+  {
+    labelHeaderNsKey: 'restfulClient',
+    url: '/restful',
+  },
+  {
+    labelHeaderNsKey: 'graphqlClient',
+    url: '/graphql',
+  },
+];
