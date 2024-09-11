90dfb6acd47d39a3c0806bee1644c3acb94491b9
Anatoli Zdanevich <anatoli.zdanevitch@gmail.com>
2024-09-12 00:33:29 +0300

    feat: add some tests with some problems

---
+++ __tests__/lngLayout.test.tsx
@@ -0,0 +1,17 @@
+// import { render, screen } from '@testing-library/react';
+// import { Params } from '@/app/i18n/data/i18n.interface';
+// import RootLayout from '@/app/[lng]/layout';
+
+// const mockParams: Params = { lng: 'en' };
+
+// const TestComponent = () => {
+//   return <div>Test Component</div>;
+// };
+
+// describe('RootLayout', () => {
+//   it('should render without errors',async () => {
+//     render(<RootLayout params={mockParams}><TestComponent /></RootLayout>);
+
+//     expect(screen.getByText('Test Component')).toBeInTheDocument();
+//   });
+// });
